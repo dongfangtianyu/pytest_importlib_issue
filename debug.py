14a09a89dd999881e4f66b5d3ad50d9f21206a9c
dongfangtianyu <7629022+dongfangtianyu@users.noreply.github.com>
2024-12-12 18:41:12 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -5,8 +5,8 @@ from _debug_external import PathFinder
 root_path = (Path('cow')).absolute()
 target_path = (Path('cow') / 'moo').absolute()
 
-spec = PathFinder.find_spec('cow.moo',[str(root_path), str(target_path)])
-print(spec)
+spec = PathFinder.find_spec('cow.moo',[str(target_path)])
+print(spec.origin)
 
 
 
